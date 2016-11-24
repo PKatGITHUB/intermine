6561ff4cc981bfe5d254f11706085bddd8b355d5
danielabutano <daniela@intermine.org>
2016-11-24 09:52:32 +0000

    Removed an unused import. Refs #1447

--- intermine/web/main/src/org/intermine/webservice/server/query/result/XMLValidator.java
+++ intermine/web/main/src/org/intermine/webservice/server/query/result/XMLValidator.java
@@ -16,7 +16,6 @@ import java.io.StringReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
 import javax.xml.XMLConstants;
 import javax.xml.transform.stream.StreamSource;
